2ba3cb9f79cb201e587d42db0fe7e25059839d0f
KreakxX <Henrik.standke@web.de>
2025-11-09 10:03:45 +0100

    Initial commit

---
+++ target/debug/deps/todo_cli-94ff54e07ecd6f2e.d
@@ -0,0 +1,5 @@
+C:\Users\Henri\Videos\todo-cli\target\debug\deps\libtodo_cli-94ff54e07ecd6f2e.rmeta: src\main.rs
+
+C:\Users\Henri\Videos\todo-cli\target\debug\deps\todo_cli-94ff54e07ecd6f2e.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/todo_cli-f463de4f461bc9e1.d
@@ -0,0 +1,5 @@
+C:\Users\Henri\Videos\todo-cli\target\debug\deps\libtodo_cli-f463de4f461bc9e1.rmeta: src\main.rs
+
+C:\Users\Henri\Videos\todo-cli\target\debug\deps\todo_cli-f463de4f461bc9e1.d: src\main.rs
+
+src\main.rs:
