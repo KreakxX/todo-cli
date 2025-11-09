6e4716e2a46860715374ea5fd83325577f98fdd1
KreakxX <Henrik.standke@web.de>
2025-11-09 10:18:00 +0100

    Todo CLI done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use dialoguer::{Input, Select, theme::ColorfulTheme};
+
 fn main() {
-    println!("Hello, world!");
+    let mut todos: Vec<String> = Vec::new();
+
+    loop {
+        let options = &["add", "view", "remove"];
+
+        let selection = Select::with_theme(&ColorfulTheme::default())
+            .with_prompt("Choose your Action")
+            .default(0)
+            .items(&options[..])
+            .interact()
+            .unwrap();
+
+        let action = options[selection];
+
+        if action == "add" {
+            println!();
+            let todo: String = Input::new()
+                .with_prompt("Input new Todo")
+                .interact_text()
+                .unwrap();
+
+            todos.push(todo)
+        }
+
+        if action == "view" {
+            todos.iter().for_each(|todo| println!("{}", todo));
+        }
+
+        if action == "remove" {
+            let selection = Select::with_theme(&ColorfulTheme::default())
+                .with_prompt("Choose your Action")
+                .default(0)
+                .items(&todos[..])
+                .interact()
+                .unwrap();
+
+            todos.remove(selection);
+        }
+    }
 }
